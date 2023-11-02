ee3456230ce4e34c9881701a3b6f1adc35a44477
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2023-11-02 09:46:10 +0000

    Sample solution

--- test_times.py
+++ test_times.py
@@ -1,12 +1,7 @@
-import datetime
-import numpy as np
+from times import compute_overlap_time, time_range
 
-from times import time_range
-from times import compute_overlap_time
-
-def test_given_input():
+def test_generic_case():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = compute_overlap_time(large, short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-    assert result == expected
+    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    assert compute_overlap_time(large, short) == expected
