616df87ad19ffb778303339b01735cca59218466
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2023-11-02 10:11:09 +0000

    Multiple tests added

--- test_times.py
+++ test_times.py
@@ -5,3 +5,21 @@ def test_generic_case():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
     assert compute_overlap_time(large, short) == expected
+
+def test_nonoverlap():
+    initial = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    final = time_range("2010-01-12 13:00:00", "2010-01-12 15:00:00")
+    expected = []
+    assert compute_overlap_time(initial, final) == expected
+
+def test_severalintervals():
+    initinterval = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 3)
+    finalinterval = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    expected = [("2010-01-12 10:30:00","2010-01-12 10:35:00"), ("2010-01-12 10:35:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00","2010-01-12 10:40:00"), ("2010-01-12 10:40:00","2010-01-12 10:45:00")]
+    assert compute_overlap_time(initinterval, finalinterval) == expected
+
+def test_endtime():
+    initial = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    final = time_range("2010-01-12 12:00:00", "2010-01-12 14:00:00")
+    expected = [("2010-01-12 12:00:00","2010-01-12 12:00:00")]
+    assert compute_overlap_time(initial, final) == expected
